38cb03d41521b85f777cb2f6c7d4f8006447be70
Steve Laskaridis <stevelaskaridis@gmail.com>
2017-09-16 15:56:53 +0200

    Get popularity of cities

--- Assets/Bridge/StationFetcher.cs
+++ Assets/Bridge/StationFetcher.cs
@@ -7,23 +7,35 @@ using System;
 
 public class StationFetcher : MonoBehaviour
 {
-    // Use this for initialization
+
+	public void Start() {
+		QueryForCitiesPopularity (null);
+	}
+
     public void QueryForCities(Action<Dictionary<int, StationData>> callback)
 	{
         // TODO: get station names, ids and xy coordinates
         // create list of stationData
         // complete info with station load
 
-        StartCoroutine(GetStationPositionData(callback));
+        StartCoroutine (GetStationPositionData (callback));
     }
 
+	public void QueryForCitiesPopularity(Action<Dictionary<int, StationData>> callback) {
+		string apiKey = "e2e01b388967105bd0911fc98c1d8a5cf133c853e645a4a7f5b0f761";
+		StartCoroutine (GetStationPopularityData (10000, 0, "asc", "dtv", apiKey, callback));
+	}
+
+	/**
+	 * Gets cities and their positions on map and calls the callback on completion.
+	 */
     IEnumerator GetStationPositionData(Action<Dictionary<int, StationData>> callback)
     {
         string apikey = "e2e01b388967105bd0911fc98c1d8a5cf133c853e645a4a7f5b0f761";
-        string rows = "rows=" + 10000;
+        string rows = "10000";
         string endpoint =
             string.Format(
-                "https://data.sbb.ch/api/records/1.0/search/?" + "apikey="+apikey + "&" + rows +
+                "https://data.sbb.ch/api/records/1.0/search/?" + "apikey="+apikey + "&" + "rows=" + rows +
                 "&dataset=didok-liste&q=zug&lang=en&facet=nummer&facet=abkuerzung&facet=tunummer&facet=tuabkuerzung&facet=betriebspunkttyp&facet=verkehrsmittel&facet=dst_abk&facet=didok&exclude.verkehrsmittel=Bus&exclude.verkehrsmittel=Luftseilbahn&exclude.verkehrsmittel=Schiff&exclude.verkehrsmittel=Tram&exclude.verkehrsmittel=Standseilbahn&exclude.verkehrsmittel=Zahnradbahn&exclude.verkehrsmittel=Metro&exclude.verkehrsmittel=Standseilbahn_Luftseilbahn&exclude.verkehrsmittel=Standseilbahn_Bus");
 
         UnityWebRequest www = UnityWebRequest.Get(endpoint);
@@ -39,21 +51,24 @@ public class StationFetcher : MonoBehaviour
             // Show results as text
             var downloadedJson = www.downloadHandler.text;
 //			Debug.Log(getCitiesNamesAndCoords(getFieldsFromJsonString(downloadedJson)).Count);
-            var results = myFoobar(downloadedJson);
+            var results = extractCitiesCoordinates(downloadedJson);
             callback(results);
             //StartCoroutine(GetStationLoadData(10000, 0, "asc", "dtv", results, apiKey, callback));
-
         }
     }
 
-    private Dictionary<int, StationData> myFoobar(string jsonString)
+	/**
+	 * Method extracting coordinates for cities.
+	 * Returns a Dictionary of <didok85, StationData> entries.
+	 */
+    private Dictionary<int, StationData> extractCitiesCoordinates(string jsonString)
     {
         var parsed = JSON.Parse(jsonString);
         var theDictList = new Dictionary<int, StationData>();
         foreach (JSONObject records in parsed["records"].AsArray)
         {
             var station = new StationData();
-            station.id = records["fields"]["nummer"];
+            station.id = records["fields"]["didok85"];
             station.name = records["fields"]["name"];
 
 			station.Position = new Vector2(records["fields"]["y_koord_ost"], records["fields"]["x_koord_nord"]);
@@ -62,8 +77,10 @@ public class StationFetcher : MonoBehaviour
         return theDictList;
     }
 
-    IEnumerator GetStationLoadData(int rowLimit, int startOffset, string sortOrder,
-		Dictionary<int, StationData> stationData, string sortingField, string apiKey,
+	/**
+	 * Gets cities and their popularities and calls the callback on completion.
+	 */
+    IEnumerator GetStationPopularityData(int rowLimit, int startOffset, string sortOrder, string sortingField, string apiKey,
         Action<Dictionary<int, StationData>> callback)
     {
         string sortingPrefix = "";
@@ -99,27 +116,66 @@ public class StationFetcher : MonoBehaviour
         {
             // Show results as text
             var downloadedJson = www.downloadHandler.text;
-//			Debug.Log(getCitiesNamesAndCoords(getFieldsFromJsonString(downloadedJson)).Count);
-            var results = getCitiesNamesAndCoords(getFieldsFromJsonString(downloadedJson), stationData);
-            Debug.Log(downloadedJson);
+            var results = getCitiesNamesAndPopularity(getFieldsFromJsonString(downloadedJson));
+			foreach (var v in results.Values)
+				Debug.Log ("Load of " + v.id + " is "+ v.load);
 
-
-            callback(null);
-
-
-//			foreach (KeyValuePair<string, Vector2> dict in ) {
-//				if (dict.Key.StartsWith("Gen") || dict.Key.StartsWith("Basel"))
-//					Debug.Log (string.Format ("{0}: {1},{2}", dict.Key, dict.Value.x, dict.Value.y));
-//			}
-//			Debug.Log(downloadedJson);
-//			foreach (KeyValuePair<string, Dictionary<string, string>> dict in getFieldsFromJsonString(downloadedJson)) {
-//				Debug.Log (string.Format ("Key: {0}", dict.Key));
-//				foreach (KeyValuePair<string, string> entry in dict.Value) {
-//					Debug.Log (string.Format("{0}: {1}", entry.Key, entry.Value));
-//				}
-//			}
+//            callback(results);
         }
     }
+
+	/**
+	 * Method extracting popularity for cities.
+	 * Returns a Dictionary of <didok85, StationData> entries.
+	 */
+	Dictionary<int, StationData> getCitiesNamesAndPopularity(Dictionary<string, Dictionary<string, string>> records)
+	{
+		var theDict = new Dictionary<int, StationData>();
+		foreach (KeyValuePair<string, Dictionary<string, string>> dict in records)
+		{
+			var record = dict.Value;
+			var station = new StationData ();
+
+			var lod_field = record ["lod"].Split (new char[]{'/'});
+			var id = int.Parse (lod_field [lod_field.Length - 1]);
+
+			station.id = id;
+			station.load = int.Parse (record ["dtv"]);
+			station.name = record ["name"];
+
+			theDict [station.id] = station;
+		}
+		return theDict;
+	}
+
+	/**
+	 * Joins the coordinated populated data with the popularity populated data
+	 */
+	Dictionary<int, StationData> joinCitiesData(Dictionary<int, StationData> citiesWithCoords, Dictionary<int, StationData> citiesWithPopularity) {
+		var lKeys = citiesWithCoords.Keys;
+		var joinedCities = new Dictionary<int, StationData> ();
+		foreach (var key in lKeys) {
+			if (citiesWithPopularity.ContainsKey(key)) {
+				joinedCities [key] = citiesWithPopularity [key];
+				joinedCities [key].Position = citiesWithCoords [key].Position;
+			}
+		}
+
+		return joinedCities;
+	}
+
+//	Dictionary<string, Vector2> getCitiesNamesAndCoords(Dictionary<string, Dictionary<string, string>> records, Dictionary<int, StationData> stationData)
+//	{
+//		var theDict = new Dictionary<string, Vector2>();
+//		foreach (KeyValuePair<string, Dictionary<string, string>> dict in records)
+//		{
+//			var record = dict.Value;
+//			theDict[record["name"]] =
+//				new Vector2(float.Parse(record["coord_x"]),
+//					float.Parse(record["coord_y"])); // TODO: replace 5 with load
+//		}
+//		return theDict;
+//	}
 
     private Dictionary<string, Dictionary<string, string>> getFieldsFromJsonString(string jsonString)
     {
@@ -135,6 +191,7 @@ public class StationFetcher : MonoBehaviour
             theDict["timestamp"] = record["record_timestamp"];
             theDict["coord_x"] = record["fields"]["x_koord_nord"];
             theDict["coord_y"] = record["fields"]["y_koord_ost"];
+			theDict["lod"] = record["fields"]["lod"];
             theDictList[theDict["code"]] = theDict;
         }
         return theDictList;
@@ -176,23 +233,10 @@ public class StationFetcher : MonoBehaviour
         return new float[] {float.Parse(records[code]["coord_x"]), float.Parse(records[code]["coord_y"])};
     }
 
-    Dictionary<string, Vector2> getCitiesNamesAndCoords(Dictionary<string, Dictionary<string, string>> records, Dictionary<int, StationData> stationData)
-    {
-        var theDict = new Dictionary<string, Vector2>();
-        foreach (KeyValuePair<string, Dictionary<string, string>> dict in records)
-        {
-            var record = dict.Value;
-            theDict[record["name"]] =
-                new Vector2(float.Parse(record["coord_x"]),
-                    float.Parse(record["coord_y"])); // TODO: replace 5 with load
-        }
-        return theDict;
-    }
-
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0, Time.unscaledTime * (0.1f), 0));
+//        gameObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0, Time.unscaledTime * (0.1f), 0));
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("clicked");
